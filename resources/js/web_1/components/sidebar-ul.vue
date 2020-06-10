0eb13679293bba438b70063c3c249916d3852448
VictorMaderaA <victor.madera.a@gmail.com>
2020-06-10 10:58:01 +0200

    Auto Collapsable menu

--- resources/js/web_1/components/sidebar-ul.vue
+++ resources/js/web_1/components/sidebar-ul.vue
@@ -1,14 +1,16 @@
 <template>
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <li class="nav-item">
-            <a href="" class="nav-link" v-on:click.stop.prevent="onCreateNewList()">
+            <a class="nav-link"
+               v-on:click.stop.prevent="onCreateNewList()">
                 <i class="fas fa-plus-circle"></i>
                 <p>Create New List</p>
             </a>
         </li>
 
         <li class="nav-item" v-for="list in lists">
-            <a href="" class="nav-link" v-on:click.stop.prevent="onListSelected(list)">
+            <a class="nav-link"
+               v-on:click.stop.prevent="onListSelected(list)">
                 <i class="far fa-circle nav-icon"></i>
                 <p>{{list.name}}</p>
             </a>
@@ -60,10 +62,20 @@
 
             onListSelected(list){
                 this.$emit('list-selected', list)
+                this.hideNavBar();
             },
             onCreateNewList(){
                 this.$emit('create-list');
+                this.hideNavBar();
+            },
+            hideNavBar(){
+                if ($(window).width() < 1000) {
+                    let element = document.getElementById("sidebar");
+                    element.classList.remove("sidebar-open");
+                    element.classList.add("sidebar-closed", "sidebar-collapse");
+                }
             }
+
         }
     }
 </script>
--- resources/views/web_1/layouts/app.blade.php
+++ resources/views/web_1/layouts/app.blade.php
@@ -24,7 +24,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 </head>
 
 
-<body class="hold-transition sidebar-mini skin-black">
+<body class="hold-transition sidebar-mini skin-black" id="sidebar">
 
     <div class="wrapper">
         <div id="app">
